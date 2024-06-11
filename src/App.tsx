71d977fca7b1b42facd5ba2a9a7a474040e28ce7
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-11 14:47:32 +0330

    2- Setting Up the Project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
 class App extends Component {
   render() {
     

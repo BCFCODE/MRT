d3afab3fdefa7acb23b5afd0c239f58f19850dd5
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-14 13:12:49 +0330

    11- Multiple Components in Sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Component } from "react";
 // import "./App.css";
 import { Counters } from "./components/Counters";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
-    return <Counters />;
+    return (
+      <>
+        <NavBar />
+        <Counters />
+      </>
+    );
   }
 }
 

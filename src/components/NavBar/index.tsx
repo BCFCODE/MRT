8f4b91b1cd1753f8336ce2d63e6214a4bc8414fe
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-16 17:25:19 +0330

    4.13 - Stateless Functional Components

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,52 +3,44 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Badge from "@mui/material/Badge";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import { Component } from "react";
 import { Button } from "@mui/material";
 import { CountersProps } from "../../types/Counters";
 
-export interface INavBarProps {
+export interface Props {
   counters: CountersProps;
   onReset: () => void;
 }
-export interface INavBarState {}
 
-class NavBar extends Component<INavBarProps, INavBarState> {
-  state = {};
+const NavBar = ({ counters, onReset }: Props) => {
+  // const numberOfValues = counters.reduce((acc, { value }) => acc + value, 0);
+  const numberOfValues = counters.reduce(
+    (acc, { value }) => (value > 0 ? acc + 1 : acc),
+    0
+  );
 
-  render() {
-    const { counters, onReset } = this.props;
-
-    // const numberOfValues = counters.reduce((acc, { value }) => acc + value, 0);
-    const numberOfValues = counters.reduce(
-      (acc, { value }) => (value > 0 ? acc + 1 : acc),
-      0
-    );
-
-    return (
-      <Box sx={{ flexGrow: 1, marginBottom: 3 }}>
-        <AppBar color="transparent" position="static">
-          <Toolbar>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "100%",
-              }}
-            >
-              <Button onClick={onReset} color="error" variant="outlined">
-                Reset
-              </Button>
-              <Badge badgeContent={numberOfValues} color="error">
-                <AddShoppingCartIcon />
-              </Badge>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
-  }
-}
+  return (
+    <Box sx={{ flexGrow: 1, marginBottom: 3 }}>
+      <AppBar color="transparent" position="static">
+        <Toolbar>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Button onClick={onReset} color="error" variant="outlined">
+              Reset
+            </Button>
+            <Badge badgeContent={numberOfValues} color="error">
+              <AddShoppingCartIcon />
+            </Badge>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
 export default NavBar;

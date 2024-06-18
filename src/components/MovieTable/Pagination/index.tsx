9d02aa6b4fb729d3de9409eb1341fc05a72c0202
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-18 20:03:47 +0330

    MoviesPagination separation of concerns bug fix

---
+++ src/components/MovieTable/Pagination/index.tsx
@@ -0,0 +1,20 @@
+import Pagination from "@mui/material/Pagination";
+
+interface Props {
+  pageCount: number
+  pageNumber: number;
+  onPageChange: (event: React.ChangeEvent<unknown>, value: number) => void;
+}
+
+const MoviesPagination = ({ pageNumber, onPageChange, pageCount }: Props) => {
+  return (
+    <Pagination
+      page={pageNumber}
+      onChange={onPageChange}
+      count={pageCount}
+      color="secondary"
+    />
+  );
+};
+
+export default MoviesPagination;
--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -5,18 +5,21 @@ import TableContainer from "@mui/material/TableContainer";
 import { Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import { Movie } from "../types/Movies";
-import MoviesPagination from "./Pagination";
 import MoviesTableHead from "./MovieTable/TableHead";
 import MoviesTableBody from "./MovieTable/TableBody";
+import Stack from "@mui/material/Stack";
+import MoviesPagination from "./MovieTable/Pagination";
 
 export interface IMoviesProps {}
 export interface IMoviesState {
   movies: Movie[];
+  page: number;
 }
 
 class Movies extends Component<IMoviesProps, IMoviesState> {
   state = {
     movies: getMovies(),
+    page: 1,
   };
 
   handleDelete = (id: string) => {
@@ -37,8 +40,13 @@ class Movies extends Component<IMoviesProps, IMoviesState> {
     }));
   };
 
+  handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    this.setState(() => ({ page: value }));
+    console.log(event, value);
+  };
+
   render() {
-    const { movies } = this.state;
+    const { movies, page } = this.state;
     const isAnyMovieInDB = movies.length;
 
     return (
@@ -63,7 +71,14 @@ class Movies extends Component<IMoviesProps, IMoviesState> {
             </Table>
           </TableContainer>
         ) : null}
-        <MoviesPagination />
+        <Stack spacing={2}>
+          <Typography>Page: {page}</Typography>
+          <MoviesPagination
+            pageNumber={page}
+            onPageChange={this.handlePageChange}
+            pageCount={10}
+          />
+        </Stack>
       </>
     );
   }

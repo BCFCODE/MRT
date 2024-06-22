bc17a296695d99e997253a0a944d6369c65c5263
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-06-22 15:12:43 +0330

    GenreList added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { Grid } from "@mui/material";
 import "./App.css";
 import { Movies } from "./components/Movies";
+import GenreList from "./components/GenreList";
 
 function App() {
-  return <Movies />;
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        <GenreList />
+      </Grid>
+      <Grid item xs={8}>
+        <Movies />
+      </Grid>
+      <Grid item xs={4}></Grid>
+      <Grid item xs={8}></Grid>
+    </Grid>
+  );
 }
 
 export default App;
